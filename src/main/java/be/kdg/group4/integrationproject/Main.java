ea3838fbc01d37d40583cda087e58659efb0b96a
J4spr <jasper07.verbruggen@gmail.com>
2026-02-04 18:57:19 +0100

    initial commit

---
+++ src/main/java/be/kdg/group4/integrationproject/Main.java
@@ -0,0 +1,11 @@
+package be.kdg.group4.integrationproject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module be.kdg.group4.integrationproject {
+    requires javafx.controls;
+    exports be.kdg.group4.integrationproject;
+}
